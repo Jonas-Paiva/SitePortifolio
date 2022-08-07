3c6a0398625fb3de1e4daf288c665f98579c95b8
Jonas Paiva <jonaspwork@gmail.com>
2022-08-07 14:41:14 -0300

    logica pronta

--- script/sobre.js
+++ script/sobre.js
@@ -1,7 +1,14 @@
 
-function test(){
+function test(buttonN){
+
+  const p = document.getElementById(`${buttonN}`);
+
   const ativo = document.getElementsByClassName('ativo')[0];
-  const desativo = document.getElementsByClassName('desativo')[0];
+
   ativo.className='desativo';
-  desativo.className = 'ativo';
+  
+  p.className ='ativo';
 }
+
+//o botão envia um a valor quando clicado para avisar qual texto teve aparecer
+// de acordo com o  botão 'value'  ativar o texto com a o id correspondente.
